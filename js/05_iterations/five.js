3c28a33890a8cc578750422ed060b7dff768c334
Abhishek Mathur <abhishekmathur200624@gmail.com>
2026-03-04 00:25:16 +0530

    Add iteration examples using forEach in five.js

---
+++ js/05_iterations/five.js
@@ -0,0 +1,20 @@
+const coding = ["js", "ruby", "python", "CPP"];
+
+coding.forEach((element) => {
+  console.log(element);
+});
+
+const mycode = [
+  {
+    language: "js",
+  },
+  {
+    language: "Java",
+  },
+  {
+    language: "py",
+  },
+];
+mycode.forEach((item) => {
+  console.log(item.language);
+});
